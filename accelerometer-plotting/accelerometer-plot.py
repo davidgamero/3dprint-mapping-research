983cdd581fc9b540f99d7fa60f055fb6ea40bff0
David Gamero <david340804@gmail.com>
2020-04-22 22:35:44 -0400

    Numerical integration with limited window

--- accelerometer-plotting/accelerometer-plot.py
+++ accelerometer-plotting/accelerometer-plot.py
@@ -5,11 +5,10 @@ import sys
 from mpl_toolkits import mplot3d
 import datetime
 import dateutil
+from mayavi import mlab
 
-print("hi")
 
-
-def parseAccelerometerDataLine(line):
+def parse_accelerometer_data_file_line(line):
     line_split = line.split(',')
 
     x = float(line_split[0])
@@ -27,7 +26,7 @@ def parseAccelerometerDataLine(line):
     return data_point
 
 
-def loadDataFromFile(path):
+def load_data_from_file(path):
     """
     Parameters:
       path : string
@@ -40,22 +39,180 @@ def loadDataFromFile(path):
     data_file = open(path)
 
     data_lines = data_file.readlines()
-    data_points = list(map(parseAccelerometerDataLine, data_lines))
+    data_points = list(map(parse_accelerometer_data_file_line, data_lines))
 
+    print('Loaded ' + str(len(data_points)) + ' data points')
     return data_points
 
 
-data = loadDataFromFile('accelerometer-plotting/accelerometer20200420-1.txt')
+def data_points_to_series(data_points):
+    """
+    Convert a list of data point dictionaries to a list for each axis over time (similar to unzip)
 
-# for i in range(len(data)):
-xdata = [point['x'] for point in data]
-ydata = [point['y'] for point in data]
-zdata = [point['z'] for point in data]
-tdata = [point['t'].timestamp() for point in data]
+    Parameters
+    ----------
+    data_points:
+        List of Dictionaries with keys:
+            'x','y','z': float
+            't': datetime
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.scatter3D(xdata, ydata, zdata, c=tdata, cmap='Blues')
+    Returns
+    ----------
+    data_series:
+        Dictionary with keys:
+            'x','y','z','t': Array of floats for each value as a series over time
+
+    """
+    xdata = [point['x'] for point in data_points]
+    ydata = [point['y'] for point in data_points]
+    zdata = [point['z'] for point in data_points]
+    tdata = [point['t'].timestamp() for point in data_points]
+
+    data_series = {
+        'x': xdata,
+        'y': ydata,
+        'z': zdata,
+        't': tdata
+    }
+
+    return data_series
+
+
+def center_data_series(data):
+    for key in ['x', 'y', 'z']:
+        data[key] = data[key] - np.mean(data[key])
+
+
+def remove_outliers_data_series(data_series, std_limit):
+    indices_to_delete = np.array([])
+
+    for key in ['x', 'y', 'z']:
+        this_series = data_series[key]
+        std = np.std(this_series)
+        new_indices_to_delete = np.argwhere(np.abs(this_series -
+                                                   np.mean(this_series)) > std_limit*std)
+        indices_to_delete = np.append(indices_to_delete, new_indices_to_delete)
+
+    for key in ['x', 'y', 'z']:
+        for index in sorted(indices_to_delete, reverse=True):
+            data_series[key] = np.delete(data_series[key], index)
+
+
+def plot_raw_accelerometer_data3d(data):
+
+    xdata = [point['x'] for point in data]
+    ydata = [point['y'] for point in data]
+    zdata = [point['z'] for point in data]
+    tdata = [point['t'].timestamp() for point in data]
+
+    fig_raw = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.scatter3D(xdata, ydata, zdata, c=tdata, cmap='Blues')
+    plt.show()
+
+    return fig_raw
+
+
+def plot_raw_accelerometer_data(data):
+
+    xdata = [point['x'] for point in data]
+    ydata = [point['y'] for point in data]
+    zdata = [point['z'] for point in data]
+    tdata = [point['t'].timestamp() for point in data]
+
+    fig_raw = plt.figure()
+
+    lim = (-2, 2)
+
+    plt.subplot(3, 1, 1)
+    plt.ylabel('X data')
+    plt.ylim(lim)
+    plt.plot(tdata, xdata)
+
+    plt.subplot(3, 1, 2)
+    plt.ylabel('Y data')
+    plt.ylim(lim)
+    plt.plot(tdata, ydata)
+
+    plt.subplot(3, 1, 3)
+    plt.ylabel('Z data')
+    plt.ylim(lim)
+    plt.plot(tdata, zdata)
+
+    plt.show()
+
+    return fig_raw
+
+
+def plot_integrate(data_series):
+    xline = np.array([])
+    yline = np.array([])
+    zline = np.array([])
+
+    x, y, z = 0, 0, 0
+    vx, vy, vz = 0.0, 0.0, 0.0
+
+    for i in range(3500):
+        dt = data_series['t'][i+1] - data_series['t'][i]
+
+        vx += data_series['x'][i]
+        vy += data_series['y'][i]
+        vz += data_series['z'][i]
+
+        x += vx * dt
+        y += vy * dt
+        z += vz * dt
+
+        xline = np.append(xline, x)
+        yline = np.append(yline, y)
+        zline = np.append(zline, z)
+
+    fig = plt.figure()
+    plt.scatter(xline, yline, color='b')
+    plt.show()
+
+    return
+
+
+def plot_integrate3d(data):
+    xline = np.array([])
+    yline = np.array([])
+    zline = np.array([])
+
+    x, y, z = 0, 0, 0
+    vx, vy, vz = 0.0, 0.0, 0.0
+
+    for i in range(500):
+        dt = data[i+1]['t'].timestamp() - data[i]['t'].timestamp()
+        data_point = data[i]
+
+        vx += data_point['x']
+        vy += data_point['y']
+        vz += data_point['z']
+
+        x += vx * dt
+        y += vy * dt
+        z += vz * dt
+
+        xline = np.append(xline, x)
+        yline = np.append(yline, y)
+        zline = np.append(zline, z)
+
+    sizeline = np.empty(len(xline)).fill(0.25)
+    s = mlab.plot3d(xline, yline, zline, tube_radius=50)
+    mlab.show()
+    return s
+
+
+data_points = load_data_from_file(
+    'accelerometer-plotting/accelerometer20200420-1.txt')
+
+data_series = data_points_to_series(data_points)
+
+center_data_series(data_series)
+#remove_outliers_data_series(data_series, 1)
+# plot_raw_accelerometer_data(data)
+plot_integrate(data_series)
 
 plt.show()
 print('done')
